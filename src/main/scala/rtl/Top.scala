9dea62b3f3707ef74d7ce46436135babb89c0f4a
Winston Lowe <wel97459@gmail.com>
2021-06-18 12:50:49 -0700

    adding top and submods

--- src/main/scala/rtl/Top.scala
+++ src/main/scala/rtl/Top.scala
@@ -5,11 +5,9 @@ import spinal.lib._
 import spinal.lib.blackbox.lattice.ice40._
 
 import Spinal1802._
+import Spinal1861._
 import MyHardware._
-import TFT_Driver._
-import _root_.MyHardware.IntOSC
-import _root_.MyHardware.RamTest
-import _root_.MyHardware.Keypad
+
 
 
 //Hardware definition
@@ -17,35 +15,25 @@ class Top extends Component {
     val io = new Bundle {
         val reset_ = in Bool
         val gpio_20 = in Bool //12Mhz CLK
-        val gpio_28 = out Bool
-        val gpio_38 = out Bool
+
+        val Video = out Bool
+        val Sync = out Bool
 
         val led_red = out Bool
         val led_green = out Bool
 
         val keypad_col = in Bits(4 bits)
         val keypad_row = out Bits(4 bits)
-
-        val lcd_rst = out Bool
-        val lcd_dc = out Bool
-        val lcd_sdi = out Bool
-        val lcd_sck = out Bool
     }
     noIoPrefix()
     
-    val tv_startFrame = Bool
-    val tv_startLine = Bool
-    val tv_dataClk = Bool
-    val tv_data = Bits(8 bits)
 
-
-    val clk75Domain = ClockDomain.internal(name = "Core75",  frequency = FixedFrequency(75 MHz))
-    val clk12Domain = ClockDomain.internal(name = "Core12",  frequency = FixedFrequency(12 MHz))
+    val clk17Domain = ClockDomain.internal(name = "Core17",  frequency = FixedFrequency(17.625 MHz))
     val clk1Domain = ClockDomain.internal(name = "Core1",  frequency = FixedFrequency(1.7625 MHz))
-    clk75Domain.setSyncronousWith(clk1Domain)
+
     //Settings 
     val PLL_CONFIG = SB_PLL40_PAD_CONFIG(
-        DIVR = B"0000", DIVF = B"0110100", DIVQ = B"011", FILTER_RANGE = B"001",
+        DIVR = B"0000", DIVF = B"0101110", DIVQ = B"101", FILTER_RANGE = B"001",
         FEEDBACK_PATH = "SIMPLE", PLLOUT_SELECT = "GENCLK", 
         DELAY_ADJUSTMENT_MODE_FEEDBACK = "FIXED", DELAY_ADJUSTMENT_MODE_RELATIVE = "FIXED", //NO DELAY
         FDA_FEEDBACK = B"0000", FDA_RELATIVE = B"0000", SHIFTREG_DIV_MODE = B"0", ENABLE_ICEGATE = False //NOT USED
@@ -57,40 +45,11 @@ class Top extends Component {
     PLL.RESETB := True
     PLL.REFERENCECLK := io.gpio_20
 
-    clk12Domain.clock := io.gpio_20
-    clk12Domain.reset := !io.reset_
+    clk17Domain.clock := PLL.PLLOUTGLOBAL
+    clk17Domain.reset := !io.reset_
 
-    val intOSC = new IntOSC()
-    intOSC.io.CLKHFEN := True
-    intOSC.io.CLKHFPU := True
-
-
-    clk75Domain.clock := PLL.PLLOUTGLOBAL
-    clk75Domain.reset := !io.reset_
-
-
-    val Core75 = new ClockingArea(clk75Domain) {
-        val startstartFrame = BufferCC(tv_startFrame, False)
-        val startLine = BufferCC(tv_startLine, False)
-        val dataClkB = BufferCC(tv_dataClk, False)
-        val dataClk = dataClkB.rise()
-        val dataClkD = RegNext(dataClk)
-        val data = RegNextWhen(tv_data, dataClk) init(0)
-
-        var TV = lcd_tv(10 ms)
-        TV.io.startFrame := startstartFrame
-        TV.io.startLine := startLine
-        TV.io.dataClk := dataClk
-        TV.io.data := data
-
-        io.lcd_rst := TV.io.lcd_rst
-        io.lcd_dc := TV.io.lcd_dc
-        io.lcd_sck := TV.io.lcd_sck
-        io.lcd_sdi := TV.io.lcd_sdi
-    }
-
-    val Core12 = new ClockingArea(clk12Domain) {
-        var div10 = CounterFreeRun(22)
+    val Core17 = new ClockingArea(clk17Domain) {
+        var div10 = CounterFreeRun(10) //Divide by 10
 
         var reset = Reg(Bool) init (False)
         var rstCounter = CounterFreeRun(100)
@@ -98,10 +57,10 @@ class Top extends Component {
             reset := True
         }
     }
-    
-    var gclk = SB_GB(Core12.div10.willOverflow)
+    //Buffer the clock signal from the divide by 10 counter
+    var gclk = SB_GB(Core17.div10.willOverflow)
     clk1Domain.clock := gclk
-    clk1Domain.reset := !Core12.reset
+    clk1Domain.reset := !Core17.reset
 
     val Core1 = new ClockingArea(clk1Domain) {
         var reset = Reg(Bool) init (False)
@@ -139,7 +98,7 @@ class Top extends Component {
                 addressRemapped := 0x1e00 | Cpu.io.Addr16(8 downto 0).resize(13)
             } otherwise (addressRemapped := Cpu.io.Addr16(12 downto 0))
 
-        val ram = new RamTest("./data/test_1861.bin", log2Up(0x1fff))
+        val ram = new Ram("./data/test_1861.bin", log2Up(0x1fff))
             ram.io.ena := True
             ram.io.wea := (!(Cpu.io.MWR || (addressRemapped.asUInt >= 0x1e00))).asBits 
             ram.io.dina := Cpu.io.DataOut
@@ -155,16 +114,12 @@ class Top extends Component {
         Cpu.io.EF_n := Cat(B"1", keypad.io.KeyOut, B"1", Pixie.io.EFx)
 
         val beeper = CounterFreeRun(1000)
-        io.gpio_38 := Pixie.io.CompSync_
-        io.gpio_28 := Pixie.io.Video
+        io.Sync := Pixie.io.CompSync_
+        io.Video := Pixie.io.Video
 
         io.led_red := !(Cpu.io.Q & beeper < 25)
         io.led_green := !(remapper & beeper < 25)
     }
-    tv_startFrame := !Core1.Pixie.io.VSync
-    tv_startLine := !Core1.Pixie.io.DMAO
-    tv_dataClk := Core1.Cpu.io.TPB && Core1.Cpu.io.SC === 2
-    tv_data := Core1.Cpu.io.DataOut
 }
 
 //Generate the MyTopLevel's Verilog
