b2c747ee20fabe608776acbd6179d9a55d7b1ed5
Winston Lowe <wel97459@gmail.com>
2024-08-11 17:24:44 -0700

    made make file work.

--- src/main/scala/rtl/Top_ice40.scala
+++ src/main/scala/rtl/Top_ice40.scala
@@ -1,16 +1,17 @@
-package rtl
+package mylib
 
 import spinal.core._
 import spinal.lib._
 import spinal.lib.blackbox.lattice.ice40._
 
 import Spinal1802._
-import MySpinalHardware._
+import Spinal1861._
 import TFT_Driver._
+import MySpinalHardware._
 
 
 //Hardware definition
-class Top_ICE40(val withLcd: Boolean, val ramFile: String, val romFile: String) extends Component {
+class Top_ice40(val withLcd: Boolean, val ramFile: String, val romFile: String) extends Component {
     val io = new Bundle {
         val reset_ = in Bool()
         val clk_12Mhz = in Bool() //12Mhz CLK
@@ -75,78 +76,35 @@ class Top_ICE40(val withLcd: Boolean, val ramFile: String, val romFile: String) 
 
         //Dived the 17.625Mhz by 10 = 1.7625Mhz
         val areaDiv = new SlowArea(10) {
-            
-            val Cpu = new Spinal1802()
-            //val Cpu = new new1802.new1802()
-            Cpu.io.Wait_n := True
-            Cpu.io.DMA_In_n := True
+            var cosmacVIP = new VIP()
+                cosmacVIP.io.reset := reset
 
-            val Pixie = new Spinal1861(10)
-            //Connection to Pixie
-            Pixie.io.DataIn := Cpu.io.DataOut
-            Pixie.io.SC := Cpu.io.SC
-            Pixie.io.TPA := Cpu.io.TPA
-            Pixie.io.TPB := Cpu.io.TPB
-            Pixie.io.Disp_On := (Cpu.io.N === 1 && Cpu.io.TPB && !Cpu.io.MWR)
-            Pixie.io.Disp_Off := (Cpu.io.N === 1 && Cpu.io.TPB && !Cpu.io.MRD)
-            Pixie.io.Reset_ := reset
-
-            //Connection to CPU from Pixie
-            Cpu.io.Interrupt_n := Pixie.io.INT
-            Cpu.io.DMA_Out_n := Pixie.io.DMAO
-            Cpu.io.Clear_n := Pixie.io.Clear
-            
             val Rom = new RamInit(romFile, log2Up(0x1ff))
                 Rom.io.ena := True
                 Rom.io.wea := 0
                 Rom.io.dina := 0x00
-                Rom.io.addra := Cpu.io.Addr16(8 downto 0)
+                Rom.io.addra := cosmacVIP.io.rom.addr
+                cosmacVIP.io.rom.data := Rom.io.douta
 
             val Ram = new RamInit(ramFile, log2Up(0x1fff))
                 Ram.io.ena := True
-                Ram.io.wea := (!Cpu.io.MWR).asBits 
-                Ram.io.dina := Cpu.io.DataOut
-                Ram.io.addra := Cpu.io.Addr16(12 downto 0)
+                Ram.io.wea := cosmacVIP.io.ram.wr.asBits
+                Ram.io.dina := cosmacVIP.io.ram.dout
+                cosmacVIP.io.ram.din := Ram.io.douta
+                Ram.io.addra := cosmacVIP.io.ram.addr
 
-            val romBootLatch = Reg(Bool) init (False)
-                //When reset, clear the latch so system boots from ROM. 
-                when(!Pixie.io.Clear) {
-                    romBootLatch := False
-                //The ROM contains the monitor program which uses a 64h instruction to pulse the N2 line.
-                } elsewhen (Cpu.io.N === 4) {
-                    romBootLatch := True
-                }
-
-                when(!romBootLatch || Cpu.io.Addr16.asUInt >= 0x8000 && Cpu.io.Addr16.asUInt <= 0x81ff) {
-                    Cpu.io.DataIn := Rom.io.douta
-                }elsewhen(Cpu.io.Addr16.asUInt < 0x2000) {
-                    Cpu.io.DataIn := Ram.io.douta
-                }otherwise{
-                    Cpu.io.DataIn := 0x00
-                }
-
-            val keypad = new Keypad()
-                keypad.io.LatchKey := (Cpu.io.N === 2 && Cpu.io.TPB && !Cpu.io.MRD)
-                keypad.io.Key := Cpu.io.DataOut(3 downto 0)
-                io.keypad.row := ~keypad.io.KeypadRow
-                keypad.io.KeypadCol := io.keypad.col
-
-            Cpu.io.EF_n := Cat(B"1", keypad.io.KeyOut, B"1", Pixie.io.EFx)
-
-            //Good beeper sounds
-            val beeper = CounterFreeRun(1000)
-            io.sync := Pixie.io.CompSync_
-            io.video := Pixie.io.Video
-
-            io.led_red := !(Cpu.io.Q & beeper < 50)
+            io.sync := cosmacVIP.io.sync
+            io.video := cosmacVIP.io.video
+            io.led_red := cosmacVIP.io.q
+            io.keypad <> cosmacVIP.io.keypad
         }
     }
 
     val lcd = ifGen(withLcd) (new Area(){    
-        val lcd_startFrame = !Core17.areaDiv.Pixie.io.INT
-        val lcd_startLine = !Core17.areaDiv.Pixie.io.DMAO
-        val lcd_dataClk = (Core17.areaDiv.Cpu.io.TPB && Core17.areaDiv.Cpu.io.SC === 2)
-        val lcd_data = Core17.areaDiv.Cpu.io.DataOut
+        val lcd_startFrame = !Core17.areaDiv.cosmacVIP.io.Pixie.INT
+        val lcd_startLine = !Core17.areaDiv.cosmacVIP.io.Pixie.DMAO
+        val lcd_dataClk = (Core17.areaDiv.cosmacVIP.io.CPU.TPB && Core17.areaDiv.cosmacVIP.io.CPU.SC === 2)
+        val lcd_data = Core17.areaDiv.cosmacVIP.io.CPU.DataOut
 
         val Core48 = new ClockingArea(clk48Domain) {
             //Clock Crossing
@@ -168,6 +126,6 @@ class Top_ICE40(val withLcd: Boolean, val ramFile: String, val romFile: String) 
     })
 }
 
-object Top_ICE40_Verilog extends App {
-  Config.spinal.generateVerilog(new Top_ICE40(true, "./data/test_1861.bin", "./data/vip.rom"))
+object Top_ice40_Verilog extends App {
+  Config.spinal.generateVerilog(new Top_ice40(true, "./data/test_1861.bin", "./data/vip.rom"))
 }
